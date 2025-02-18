147c93924af0fadcb680a088d20eca91d5f24950
KiziDooh <131369977+KiziDooh@users.noreply.github.com>
2025-02-17 21:14:03 -0600

    idk rays r confusing me

--- scenes/bunny_mean_dance.gd
+++ scenes/bunny_mean_dance.gd
@@ -1,11 +1,6 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
+const SPEED = -60
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position.z += SPEED * delta
---
+++ scenes/ray_cast_3d.gd
@@ -0,0 +1,15 @@
+extends RayCast3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("super")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func process(delta):
+	#if RayCast3D.is_colliding():
+		#print("hello")
+#	if raycast.is_colliding():
+#		print("super")
