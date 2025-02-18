4947cfd03eb7e1521b3b47a1bfa6cbe114a7635f
KiziDooh <131369977+KiziDooh@users.noreply.github.com>
2025-02-17 21:16:11 -0600

    v

--- scenes/bunny_mean_dance.gd
+++ scenes/bunny_mean_dance.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-const SPEED = -60
+const SPEED = -15
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.z += SPEED * delta
