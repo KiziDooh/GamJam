6a50c5dfb6d2371e1dd091b4388e8074bf694811
KiziDooh <131369977+KiziDooh@users.noreply.github.com>
2025-02-19 04:39:08 -0600

    MENU and we have rays working

--- scenes/scripts/ray_cast_3d.gd
+++ scenes/scripts/ray_cast_3d.gd
@@ -4,12 +4,8 @@ extends RayCast3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("super")
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func process(delta):
-#	if raycast.is_colliding():
-		#print("hello")
-#	if raycast.is_colliding():
-#		print("super")
+func _process(delta):
+	if is_colliding():
+		print(get_collider())
