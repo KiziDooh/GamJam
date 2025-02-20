ee147fdc255fb4ecdbc9573175b1f13bef74b8ca
KiziDooh <131369977+KiziDooh@users.noreply.github.com>
2025-02-19 18:10:14 -0600

    push

---
+++ scenes/models/knife.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+var Knife = true
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		if Knife == true:
+			visible = true
--- scenes/scripts/ray_cast_3d.gd
+++ scenes/scripts/ray_cast_3d.gd
@@ -1,6 +1,7 @@
 extends RayCast3D
 
-
+#var var1 = "CAMPFIRE:<Area3D#48972695712>"
+var var2 = "sda"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("super")
@@ -9,3 +10,5 @@ func _ready():
 func _process(delta):
 	if is_colliding():
 		print(get_collider())
+	#	if type_string(var2) == type_string(var1):
+			#print("yup")
