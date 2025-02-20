1f1a181a4aa79736530af408ea2390a046e3f3a7
KiziDooh <131369977+KiziDooh@users.noreply.github.com>
2025-02-19 18:56:24 -0600

    can make evil bunny go to a postion

--- scenes/models/evil_gilbert.gd
+++ scenes/models/evil_gilbert.gd
@@ -1,7 +1,15 @@
 extends Node3D
-
+var posZ = 0
+var posX = 0
 const SPEED = -10
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.z += SPEED * delta
+	if position.z < posZ:
+		position.z -= SPEED * delta
+	if position.z > posZ:
+		position.z += SPEED * delta
+	if position.x < posX:
+		position.x -= SPEED * delta
+	if position.x > posX:
+		position.x += SPEED * delta
