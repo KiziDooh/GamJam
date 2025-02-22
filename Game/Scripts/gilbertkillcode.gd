95b27367d35b48926b6fc35878fc7fbce8a8a4e1
TPhobophobia <108951596+TPhobophobia@users.noreply.github.com>
2025-02-21 22:30:27 -0600

    Transition

--- Game/Scripts/gilbertkillcode.gd
+++ Game/Scripts/gilbertkillcode.gd
@@ -5,14 +5,28 @@ extends Node3D
 func _ready():
 	pass # Replace with function body.
 
+var cooldown: float = 0
+var cooldown2: float = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if Global.gilbertded == true:
+		if cooldown >= 0:
+			cooldown -= delta
+		elif cooldown <= 0:
+			$"../../TextureRect".visible = true
+			cooldown = 0.2
+		if cooldown2 >= 0:
+			cooldown2 -= delta
+		elif cooldown2 <= 0:
+			$"../../TextureRect".visible = false
+			cooldown2 = 0.3
+		
 func _process(delta):
 	if Global.yup == true:
 		if ((Global.get.name == "Bunny") and Input.is_key_pressed(KEY_E)) and Global.knhold == true:
 			print("dead")
 			Global.gilbertded = true
-			$"../../TextureRect".visible = true
 			$"../../Glitch Effect".visible = true
 			$"gilbert head/Timer".start()
 		elif ((Global.get.name == "Bunny") and Input.is_key_pressed(KEY_E)):
